b90c2bb939908489992f5dd8b5873ca135f2d17c
johnwatkins0 <johnwatkins0@gmail.com>
2018-04-28 19:58:37 -0400

    Try branch library

--- dist/getGitBranch.js
+++ dist/getGitBranch.js
@@ -5,27 +5,44 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.getGitBranch = undefined;
 
-var _child_process = require('child_process');
+var _gitBranch = require('git-branch');
 
-var _ = require('.');
+var _gitBranch2 = _interopRequireDefault(_gitBranch);
 
-global.GIT_BRANCH = null;
-global.GIT_GET_BRANCH_COMMAND = 'git rev-parse --abbrev-ref HEAD';
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 var getGitBranch = exports.getGitBranch = function getGitBranch() {
-  return new Promise(function (resolve, reject) {
-    if (global.GIT_BRANCH !== null) {
-      resolve(global.GIT_BRANCH);
-    }
+  return new Promise(function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(resolve) {
+      var branchName;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              if (global.GIT_BRANCH !== null) {
+                resolve(global.GIT_BRANCH);
+              }
 
-    (0, _child_process.exec)(global.GIT_GET_BRANCH_COMMAND, _.COMMAND_DEFAULTS, function (error, stdout) {
-      if (error) {
-        reject('ERROR: The current git branch could not be retrieved.');
-        return;
-      }
+              _context.next = 3;
+              return (0, _gitBranch2.default)();
 
-      global.GIT_BRANCH = stdout.trim();
-      resolve(global.GIT_BRANCH);
-    });
-  });
+            case 3:
+              branchName = _context.sent;
+
+              resolve(branchName);
+
+            case 5:
+            case 'end':
+              return _context.stop();
+          }
+        }
+      }, _callee, undefined);
+    }));
+
+    return function (_x) {
+      return _ref.apply(this, arguments);
+    };
+  }());
 };
