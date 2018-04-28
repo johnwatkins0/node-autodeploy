b04ad40a1dee85001695982d45f2094818171fbf
johnwatkins0 <johnwatkins0@gmail.com>
2018-04-28 19:47:45 -0400

    Resolve tests

--- src/__tests__/maybeDeploy.test.js
+++ src/__tests__/maybeDeploy.test.js
@@ -6,28 +6,37 @@ const { deployConfig, serverConfig } = global;
 
 describe('maybeDeploy function', () => {
   it('handles switched off deployment', async () => {
+    const branch = global.GIT_BRANCH;
+    global.GIT_BRANCH = 'HEAD';
     const message = await maybeDeploy(serverConfig, deployConfig);
 
-    expect(message).toBe("The current branch is master.\nNot deploying to master. It's not in .deploy-servers.js");
+    expect(message).toBe("The current branch is HEAD.\nNot deploying to HEAD. It's not in .deploy-servers.js");
+    global.GIT_BRANCH = branch;
   });
 
   it('handles deployment but times out', async () => {
+    const branch = global.GIT_BRANCH;
+    global.GIT_BRANCH = 'HEAD';
     COMMAND_DEFAULTS.timeout = 2000;
     const serverConfig2 = { ...serverConfig };
     serverConfig2.HEAD.active = true;
     const message = await maybeDeploy(serverConfig2, deployConfig);
+    global.GIT_BRANCH = branch;
 
-    expect(message).toMatch(/The current branch is master./);
+    expect(message).toMatch(/The current branch is HEAD/);
   });
 
   it('works with a port number', async () => {
+    const branch = global.GIT_BRANCH;
+    global.GIT_BRANCH = 'HEAD';
     COMMAND_DEFAULTS.timeout = 2000;
     const serverConfig2 = { ...serverConfig };
     serverConfig2.HEAD.active = true;
     serverConfig2.HEAD.port = 20;
     const message = await maybeDeploy(serverConfig2, deployConfig);
 
-    expect(message).toMatch(/The current branch is master./);
+    expect(message).toMatch(/The current branch is HEAD./);
+    global.GIT_BRANCH = branch;
   });
 
   it('handles deployment successfully', async () => {
